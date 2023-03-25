330cceb4463727c6d47a6149614baabe7bdd6b37
Carolista <cjones.renaissancewoman@gmail.com>
2023-03-25 06:49:03 -0500

    removed unused model param from POST handlers

--- src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/controllers/AuthenticationController.java
@@ -62,8 +62,7 @@ public class AuthenticationController {
     @PostMapping("/register")
     public String processRegistrationForm(@ModelAttribute @Valid RegistrationFormDTO registrationFormDTO,
                                           Errors errors,
-                                          HttpServletRequest request,
-                                          Model model) {
+                                          HttpServletRequest request) {
 
         // Send user back to form if errors are found
         if (errors.hasErrors()) {
@@ -104,8 +103,7 @@ public class AuthenticationController {
     @PostMapping("/login")
     public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
                                    Errors errors,
-                                   HttpServletRequest request,
-                                   Model model) {
+                                   HttpServletRequest request) {
 
         // Send user back to form if errors are found
         if (errors.hasErrors()) {
